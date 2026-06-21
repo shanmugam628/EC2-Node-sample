6b95f6a43a1db2dac43e7ac1550872423c0ab4fc
shanmugam <vmshanmugam48@gmail.com>
2026-06-21 23:18:10 +0530

    new code Modified

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 {
-  "name": "node-app",
+  "name": "bmw-branding-site",
   "version": "1.0.0",
+  "description": "BMW Branding Website using Node.js",
   "main": "server.js",
   "scripts": {
     "start": "node server.js"
   },
   "dependencies": {
-    "express": "^5.0.0"
+    "express": "^4.19.2"
   }
 }
